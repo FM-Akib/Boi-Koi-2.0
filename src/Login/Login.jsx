d6469bf10f80f01b7650234b8a87f27197a7cf5f
FM-Akib <muntasirakib08@gmail.com>
2024-02-15 01:37:01 +0600

    login ui updated

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,7 +1,9 @@
 
-
+import { useState } from "react";
+import { IoEye } from "react-icons/io5";
+import { IoEyeOff } from "react-icons/io5";
 const Login = () => {
-
+const [show,setshow]= useState(false);
 
 const loginOnSubmit = (e) =>{
     const email=e.target.email.value;
@@ -9,8 +11,9 @@ const loginOnSubmit = (e) =>{
       console.log(email,password);
 }
 
-    return (
-        <div className="bg-yellow-400 dark:bg-yellow-400 h-screen overflow-hidden flex items-center justify-center">
+return (
+
+<div className="bg-yellow-400 dark:bg-yellow-400 h-screen overflow-hidden flex items-center justify-center">
   <div className="bg-white lg:w-6/12 md:7/12 w-8/12 shadow-3xl rounded-xl mt-14">
     <div className="bg-gray-800 shadow shadow-gray-200 absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-full p-4 md:p-8">
       <svg width="32" height="32" viewBox="0 0 24 24" fill="#FFF">
@@ -26,11 +29,31 @@ const loginOnSubmit = (e) =>{
         </svg>
         <input name="email" type="email" id="username" className="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Username" />
       </div>
-      <div className="flex items-center text-lg mb-6 md:mb-8">
+
+
+
+      <div className="relative flex items-center text-lg mb-6 md:mb-8">
         <svg className="absolute ml-3" viewBox="0 0 24 24" width="24">
           <path d="m18.75 9h-.75v-3c0-3.309-2.691-6-6-6s-6 2.691-6 6v3h-.75c-1.24 0-2.25 1.009-2.25 2.25v10.5c0 1.241 1.01 2.25 2.25 2.25h13.5c1.24 0 2.25-1.009 2.25-2.25v-10.5c0-1.241-1.01-2.25-2.25-2.25zm-10.75-3c0-2.206 1.794-4 4-4s4 1.794 4 4v3h-8zm5 10.722v2.278c0 .552-.447 1-1 1s-1-.448-1-1v-2.278c-.595-.347-1-.985-1-1.722 0-1.103.897-2 2-2s2 .897 2 2c0 .737-.405 1.375-1 1.722z"/>
         </svg>
-        <input name="password" type="password" id="password" className="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Password" />
+        
+     
+
+      <input 
+        name="password" 
+        type={show?"text":"password"} 
+        id="password" 
+        className="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Password"/> 
+        <span onClick={() =>setshow(!show)} className="cursor-pointer absolute right-3 top-5">
+          {
+              show? <IoEyeOff /> : <IoEye />
+          }
+          </span>
+
+
+     
+
+
       </div>
       <button className="bg-gradient-to-b from-gray-700 to-gray-900 font-medium p-2 md:p-4 text-white uppercase w-full rounded">Login</button>
     </form>
